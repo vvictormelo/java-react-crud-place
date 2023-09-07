770295494bdb19d85fe59cba4d7bbc851a80561c
Victor Melo <melovictor515@gmail.com>
2023-09-07 12:51:28 -0300

    project created

---
+++ crud-place/src/main/java/br/com/vvictormelo/crudplace/CrudPlaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.vvictormelo.crudplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPlaceApplication.class, args);
+	}
+
+}
---
+++ crud-place/src/test/java/br/com/vvictormelo/crudplace/CrudPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vvictormelo.crudplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
