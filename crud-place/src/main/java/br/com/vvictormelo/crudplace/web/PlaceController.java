f1bf49c5ca5c35c3ceb3052a3a276bfdf53551af
Victor Melo <melovictor515@gmail.com>
2023-09-07 15:44:31 -0300

    database created

--- crud-place/src/main/java/br/com/vvictormelo/crudplace/web/PlaceController.java
+++ crud-place/src/main/java/br/com/vvictormelo/crudplace/web/PlaceController.java
@@ -5,6 +5,7 @@ import br.com.vvictormelo.crudplace.domain.PlaceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -21,7 +22,7 @@ public class PlaceController {
     }
     
     @PostMapping
-    public ResponseEntity<Mono<Place>> create(Place place) {
+    public ResponseEntity<Mono<Place>> create(@RequestBody Place place) {
         var createdPlace = placeService.create(place);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
     }
